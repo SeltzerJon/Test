6071ee99f88019dda9d6c78ab3d09a6091ffd404
Jonathan <bestfriends90@gmail.com>
2023-05-03 00:30:15 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# # Machine Learning
+import numpy as np
+
+# arr = np.array([1, 2, 3, 4, 5])
+#
+# print(arr)
+# ----------------------------
+# Create a NumPy ndarray Object
+
+# arr = np.array([1, 2, 3, 4, 5])
+#
+# print(arr)
+# # Type of the object passed
+# print(type(arr))
+
+
+# arr = np.array((1, 2, 3, 4, 5))
+#
+# print(arr)
+# --------------------------------
+# 0-D Arrays
+arr = np.array(42)
+print(arr)
